ad94c2335a8148e7ae1f7b0a9b349659d6948132
Angela Duran <duranangela19@gmail.com>
2018-05-24 09:50:09 -0600

    Initial commit, iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,14 @@
 class Book
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
 
+  def initialize(args = {})
+    @author_first_name = args.fetch(:author_first_name, '')
+    @author_last_name = args.fetch(:author_last_name, '')
+    @title = args.fetch(:title, '')
+    @publication_date = args.fetch(:publication_date, '')[-4..-1]
+  end
 
 end
